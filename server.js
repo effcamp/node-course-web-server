cfead44a0dad8f6f6647fd5a73b67a56e90c8ba4
effcamp <fcf.camposx@gmail.com>
2017-11-27 21:17:42 -0200

    Added Projects page

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.get('/about', (req, res) => {
     pageTitle: 'About Page'
   })
 })
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    projectMessage: 'This is my portfolio.',
+    pageTitle: 'Projects'
+  })
+})
 app.get('/bad', (req, res) => {
   res.send({ errorMessage: 'Unable to handle request' })
 })
